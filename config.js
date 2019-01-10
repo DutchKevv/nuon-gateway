cfe9e776cc6d213f9526af45d8647b03212068ec
Kewin Brandsma <kewin@frontend-freelance.com>
2019-01-10 19:05:55 +0100

    wip

--- config.js
+++ config.js
@@ -2,13 +2,9 @@ module.exports = {
     server: {
         cloud: {
             protocol: 'http://',
-            host: 'localhost',
-            // host: '136.144.181.63',
+            // host: 'localhost',
+            host: '136.144.181.63',
             port: 5000
-        },
-        work: {
-            protocol: 'http://',
-            port: 5001
         }
     }
 }
