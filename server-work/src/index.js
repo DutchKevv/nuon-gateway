8cb3cae23afeeb86178b0da99f138b70ef7ef1cf
Brandsma Kewin (YIANA) ext <afn47@eur.corp.vattenfall.com>
2019-04-20 18:09:59 +0200

    w

--- server-work/src/index.js
+++ server-work/src/index.js
@@ -1,23 +1,16 @@
 // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
-
-const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const app = express();
 const request = require('requestretry');
 const io = require('socket.io-client');
 const HttpProxyAgent = require('http-proxy-agent');
-
-const port = 3000;
 
 const URL_DU1 = 'https://brandsmak:Halo33221!@sapdu1.corp.vattenfall.com';
 const URL_PROXY = 'http://afn47:Halo33221!@proxy-nl.corp.vattenfall.com:8080';
 const URL_GATEWAY = 'http://136.144.181.63:5111';
 
-// const r = request.defaults({ 'proxy': 'http://afn47:Halo33221!@proxy-nl.corp.vattenfall.com:8080' });
-
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// needed to get valid CSRF tokens from SAP (hack)
+let cookies = '';
 
 const socket = io(URL_GATEWAY, {
   secure: false,
@@ -31,7 +24,6 @@ const socket = io(URL_GATEWAY, {
 
 socket.on('connect', () => {
   console.log('connect!');
-
   socket.emit('post:init', { type: 'work' })
 });
 
@@ -39,10 +31,7 @@ socket.on('connect_error', (error) => {
   console.error('connection error:', error);
 });
 
-socket.on('get:api', (data, cb) => {
-  console.log('\nheaders', data);
-  // console.log('\nforwading:', URL_DU1 + data.url);
-  
+socket.on('get:api', (data, cb) => {  
   const method = data.method.toLowerCase();
 
   const options = {
@@ -51,16 +40,20 @@ socket.on('get:api', (data, cb) => {
     query: data.query,
     url: URL_DU1 + data.url,
     ca: fs.readFileSync(path.join(__dirname, '../certs/ca.cert.pem')),
-    maxAttempts: 1,
+    maxAttempts: 3,
     strictSSL: false,
-    headers: data.headers,
+    headers: {
+      ...data.headers,
+      Cookie: cookies
+    },
   };
 
   delete options.headers['accept-encoding'];
 
   request(options, function (err, response, body) {
-    if (response) {
-      console.log(response.headers)
+    // store SAP session cookies
+    if (response.headers['set-cookie']) {
+      cookies = response.headers['set-cookie'];
     }
 
     if (err) {
