e33e99306288136d9e903a765ec1dbc0b58dba8f
Kewin Brandsma <kewin@frontend-freelance.com>
2019-07-30 05:19:42 +0200

    sdfdsf

--- server-cloud/src/index.js
+++ server-cloud/src/index.js
@@ -37,8 +37,7 @@ app.all('*', function (req, res) {
         cookies: req.cookies
     }, (result) => {
         console.log('api returned from worker');
-        res.header("Content-Type", "text/xml");
-        res.status(result.statusCode || 504).send(result.body);
+        res.set(result.headers).status(result.statusCode || 504).send(result.body);
     });
 });
 
