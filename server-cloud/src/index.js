73e5c1628c409e4b01f2cf13ff1119616ba3e9ab
Kewin Brandsma <kewin@frontend-freelance.com>
2019-07-30 04:27:08 +0200

    sadfsdf

--- server-cloud/src/index.js
+++ server-cloud/src/index.js
@@ -13,8 +13,9 @@ const sockets = {
 
 server.listen(PORT);
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.raw({type: () => true}));
 app.use(cookieParser());
 
 app.get('/', function (req, res) {
